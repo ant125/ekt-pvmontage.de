f2bede5fc7fb387870aaa090bc44e5634031049e
ant125 <ant1255@gmail.com>
2026-04-15 07:12:24 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,7 +115,7 @@ export default function Page() {
             <img
               src="/hero-worker.png"
               alt="Besik Gigauri – Photovoltaik-Montage"
-              className="h-auto max-h-[min(62vh,640px)] w-auto max-w-full object-contain object-bottom object-right md:max-h-[min(78vh,860px)]"
+              className="h-auto max-h-[min(78vh,760px)] w-auto max-w-none object-contain object-bottom object-right md:max-h-[min(90vh,920px)] md:translate-x-10"
             />
           </div>
         </div>
